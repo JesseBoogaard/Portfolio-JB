6dc7ff5c7e30de779ca8b3f8d49aca2e698347fa
Jesse Boogaard <jesse98@live.nl>
2018-10-20 14:41:33 +0200

    removed social button from footer and instead added a floating button

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,11 @@
         </v-layout>
       </v-container>
     </v-parallax> -->
+      <v-fab-transition>
+        <v-btn @click.native="socials = true;" style="margin-bottom: 42px;" color="secondary" fixed dark absolute bottom right fab>
+          <v-icon>add</v-icon>
+        </v-btn>
+      </v-fab-transition>
     <v-toolbar flat app dense>
       <v-btn depressed icon ripple href="/#/">
         <v-img src="static/img/favicon.png" aspect-ratio="1"></v-img>
@@ -32,13 +37,13 @@
     <v-content>
       <router-view/>
     </v-content>
-    <v-layout row justify-center>
+  <v-layout row justify-center>
     <v-dialog v-model="socials" max-width="500">
       <v-card>
-        <v-toolbar color="secondary" dense flat depressed>
+        <v-toolbar color="secondary" flat depressed>
           <v-toolbar-title>
             <div>
-              <h3 class="headline">Voeg mij toe!</h3>
+              <h2 class="headline">Voeg mij toe!</h2>
             </div>
           </v-toolbar-title>
         </v-toolbar>
@@ -56,8 +61,6 @@
   </v-layout>
     <v-footer app absolute>
       <span style="padding-left: 2%;">Jesse Boogaard &copy; {{ new Date().getFullYear() }}</span>
-      <v-spacer></v-spacer>
-      <v-icon @click.native="socials = true;" style="padding-right: 2%;">person_add</v-icon>
     </v-footer>
   </v-app>
 </template>
