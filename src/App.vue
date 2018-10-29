5a523aa13144b260c3eb167162b78c2d2b334aaa
I403755 <j.boogaard@student.fontys.nl>
2018-10-29 09:14:00 +0100

    removed school related projects and info from site

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <template>
   <v-app dark>
-    <!-- <v-parallax height="380" dark src="/static/img/banner.jpg">
-      <v-container fill-height>
-        <v-layout align-center>
-          <v-flex>
-            <h3 class="display-2" align="center">Portfolio Jesse Boogaard</h3>
-            <v-divider class="my-3"></v-divider>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-parallax> -->
       <v-fab-transition>
         <v-btn @click.native="socials = true;" style="margin-bottom: 42px;" color="primary" fixed dark absolute bottom right fab>
           <v-icon>person_add</v-icon>
@@ -18,23 +8,6 @@
     <v-toolbar flat app dense>
       <v-btn depressed icon ripple href="/#/">
         <v-img src="static/img/favicon.png" aspect-ratio="1"></v-img>
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn flat href="/#/">
-        home
-      </v-btn>
-      <v-menu offset-y>
-        <v-btn flat slot="activator">
-          vakken
-        </v-btn>
-        <v-list>
-          <v-list-tile v-for="(subject, index) in subjects" :key="index" :href="subject.url">
-            <v-list-tile-title>{{ subject.title }}</v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-menu>
-      <v-btn flat href='/#/downloads'>
-        downloads
       </v-btn>
     </v-toolbar>
     <v-content>
@@ -73,13 +46,6 @@ export default {
   data () {
     return {
       socials: false,
-      subjects: [
-        { title: 'Media', url: '/#/media' },
-        { title: 'Software', url: '/#/software' },
-        { title: 'Technology', url: '/#/technology' },
-        { title: 'Business', url: '/#/business' },
-        { title: 'Portfolio', url: '/#/portfolio' }
-      ],
       media: [
         { name: 'LinkedIn', url: 'https://www.linkedin.com/in/jesseboogaard/', icon: 'group_add' },
         { name: 'GitHub', url: 'https://www.github.com/JesseBoogaard', icon: 'code' },
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/components/home'
-import downloads from '@/components/downloads'
-import media from '@/components/media'
-import business from '@/components/business'
-import software from '@/components/software'
-import technology from '@/components/technology'
-import portfolio from '@/components/portfolio'
 
 Vue.use(Router)
 
@@ -16,36 +10,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: home
-    },
-    {
-      path: '/downloads',
-      name: 'downloads',
-      component: downloads
-    },
-    {
-      path: '/media',
-      name: 'media',
-      component: media
-    },
-    {
-      path: '/business',
-      name: 'business',
-      component: business
-    },
-    {
-      path: '/software',
-      name: 'software',
-      component: software
-    },
-    {
-      path: '/technology',
-      name: 'technology',
-      component: technology
-    },
-    {
-      path: '/portfolio',
-      name: 'portfolio',
-      component: portfolio
     }
   ]
 })
